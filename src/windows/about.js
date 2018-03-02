589f5dfb12d5ada8910fc81de8b50e1f394b15b3
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-03-02 22:24:52 +0100

    Make windows' views

--- src/windows/about.js
+++ src/windows/about.js
@@ -1,12 +1,22 @@
 const { BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
 module.exports = {
     ctx: null,
     init() {
-        return this.ctx = new BrowserWindow({
+        this.ctx = new BrowserWindow({
             show: false,
-            width: 400,
-            height: 600,
+            width: 300,
+            height: 380,
         })
+
+        this.ctx.loadURL(url.format({
+            pathname: path.join(`${__dirname}/../views`, 'about.html'),
+            protocol: 'file:',
+            slashes: true,
+        }))
+
+        return this.ctx
     },
 }
