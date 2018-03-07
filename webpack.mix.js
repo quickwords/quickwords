fb2e211de7d4fe8b94420f6c13d68c31006ea85c
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-03-08 00:48:14 +0100

    Electron builder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix')
 
-mix.sass('src/styles/app.sass', 'build/styles/app.css')
-mix.sass('src/windows/about/styles.sass', 'build/styles/about.css')
-mix.sass('src/windows/preferences/styles.sass', 'build/styles/preferences.css')
+mix.sass('src/styles/app.sass', 'src/styles/app.css')
+mix.sass('src/windows/about/styles.sass', 'src/windows/about/styles.css')
+mix.sass('src/windows/preferences/styles.sass', 'src/windows/preferences/styles.css')
