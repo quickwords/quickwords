9b353f62caac182bb2b197f89f94ecd45cb92241
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-20 22:59:41 +0200

    Default snippets - fix

--- src/modules/defaultSnippets.js
+++ src/modules/defaultSnippets.js
@@ -14,16 +14,9 @@ module.exports = [
         'type': 'plain'
     },
     {
-        'id': 3,
-        'key': ';edit .* ',
-        'value': "// It opens a file you provide it in your default editor\n// Just try typing ';edit ~/hello.txt ' (note the trailing space)\nfunction (trigger) {\n    return new Promise((resolve) => {\n        const filename = trigger.split(' ')[1]\n\n        const exec = require('child_process').exec\n        exec(`\\`$EDITOR ${filename}\\``)\n        \n        setTimeout(() => resolve(''), 100)\n    })\n}\n",
-        'regex': true,
-        'type': 'js'
-    },
-    {
         'id': 4,
         'key': ';up .* ',
-        'value': "// After typing everything after ';up ' becomes uppercase (note the trailing space)\nfunction (trigger) {\n    return trigger.substr(4).toUpperCase()\n}\n",
+        'value': "// After typing everything after ';up ' becomes uppercase\n// Note that Quickwords matches only last 20\n// characters, so anything above that will not work\nfunction (trigger) {\n    return trigger.substr(4).toUpperCase()\n}\n",
         'regex': true,
         'type': 'js'
     }
