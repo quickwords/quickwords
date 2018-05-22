d8d1ad25ef24f4e37d3d44119a187cd0630bc483
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-22 19:05:49 +0200

    Remove purge and disable uglify

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,6 @@
 const mix = require('laravel-mix')
 const path = require('path')
 const tailwindcss = require('tailwindcss')
-const glob = require('glob-all')
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-
-class TailwindExtractor {
-    static extract(content) {
-        return content.match(/[A-z0-9-:\/]+/g) || [];
-    }
-}
 
 mix
     .copyDirectory('src/main', 'out/main')
@@ -28,27 +20,5 @@ mix
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss(path.join(__dirname, './tailwind.js')) ],
+        uglify: false,
     })
-
-if (mix.inProduction()) {
-    mix.webpackConfig({
-        plugins: [
-            new PurgecssPlugin({
-                // Specify the locations of any files you want to scan for class names.
-                paths: glob.sync([
-                    path.join(__dirname, 'src/renderer/windows/**/*.html'),
-                    path.join(__dirname, 'src/renderer/windows/**/*.js'),
-                ]),
-                extractors: [
-                    {
-                        extractor: TailwindExtractor,
-
-                        // Specify the file extensions to include when scanning for
-                        // class names.
-                        extensions: ['html', 'js', 'vue'],
-                    },
-                ],
-            }),
-        ],
-    });
-}
