6aabb16d9252d1b4d72b255fdb2f034f891da142
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-22 14:33:53 +0200

    Better looking export

--- src/main/helpers.js
+++ src/main/helpers.js
@@ -2,95 +2,96 @@ const { Notification, shell, Menu } = require('electron')
 const fetch = require('node-fetch')
 const path = require('path')
 
-module.exports = {
-    doNotQuitAppOnWindowClosure(windows) {
-        Object.keys(windows).forEach(key => windows[key].on('close', e => {
-            e.preventDefault()
-            windows[key].hide()
-        }))
-    },
-    unregisterWindowListeners(windows) {
-        Object.keys(windows).forEach(key => windows[key].removeAllListeners('close'))
-    },
-    async checkForNewVersion() {
-        const currentVersion = require('../package.json').version.split('.')
+module.exports.doNotQuitAppOnWindowClosure = function (windows) {
+    Object.keys(windows).forEach(key => windows[key].on('close', e => {
+        e.preventDefault()
+        windows[key].hide()
+    }))
+}
 
-        let response
-        let data
+module.exports.unregisterWindowListeners = function (windows) {
+    Object.keys(windows).forEach(key => windows[key].removeAllListeners('close'))
+}
 
-        try {
-            response = await fetch('https://api.github.com/repos/quickwords/quickwords/releases/latest')
-            data = await response.json()
-        } catch (e) {
-            return false
-        }
+module.exports.checkForNewVersion = async function () {
+    const currentVersion = require('../package.json').version.split('.')
 
-        const currentNewestVersion = data.tag_name.split('.')
-        const url = data.html_url
+    let response
+    let data
 
-        if (
-            currentNewestVersion[0] > currentVersion[0]
-            || (currentNewestVersion[0] === currentVersion[0] && currentNewestVersion[1] > currentVersion[1])
-            || (currentNewestVersion[0] === currentVersion[0] && currentNewestVersion[1] === currentVersion[1] && currentNewestVersion[2] > currentVersion[2])
-        ) {
-            const notification = new Notification({
-                title: 'New Version Available',
-                body: `Version ${currentNewestVersion.join('.')} of Quickwords is available`,
-                icon: path.join(__dirname, '../../build/icon.icns'),
-            })
-
-            notification.on('click', () => shell.openExternal(url))
-
-            notification.show()
-
-            return true
-        }
-
+    try {
+        response = await fetch('https://api.github.com/repos/quickwords/quickwords/releases/latest')
+        data = await response.json()
+    } catch (e) {
         return false
-    },
-    registerNativeShortcuts(app, windows) {
-        // This menu does not show up nowhere, but it does register shortcuts like copy-paste, close and minimize
-        Menu.setApplicationMenu(Menu.buildFromTemplate([
-            {
-                label: 'Quickwords',
-                submenu: [
-                    {
-                        label: 'About Quickwords',
-                        click() {
-                            windows.about.show()
-                        },
+    }
+
+    const currentNewestVersion = data.tag_name.split('.')
+    const url = data.html_url
+
+    if (
+        currentNewestVersion[0] > currentVersion[0]
+        || (currentNewestVersion[0] === currentVersion[0] && currentNewestVersion[1] > currentVersion[1])
+        || (currentNewestVersion[0] === currentVersion[0] && currentNewestVersion[1] === currentVersion[1] && currentNewestVersion[2] > currentVersion[2])
+    ) {
+        const notification = new Notification({
+            title: 'New Version Available',
+            body: `Version ${currentNewestVersion.join('.')} of Quickwords is available`,
+            icon: path.join(__dirname, '../../build/icon.icns'),
+        })
+
+        notification.on('click', () => shell.openExternal(url))
+
+        notification.show()
+
+        return true
+    }
+
+    return false
+}
+
+module.exports.registerNativeShortcuts = function (app, windows) {
+    // This menu does not show up nowhere, but it does register shortcuts like copy-paste, close and minimize
+    Menu.setApplicationMenu(Menu.buildFromTemplate([
+        {
+            label: 'Quickwords',
+            submenu: [
+                {
+                    label: 'About Quickwords',
+                    click() {
+                        windows.about.show()
                     },
-                    { type: 'separator' },
-                    {
-                        label: 'Quit',
-                        accelerator: 'Command+Q',
-                        click() {
-                            app.quit()
-                        },
+                },
+                { type: 'separator' },
+                {
+                    label: 'Quit',
+                    accelerator: 'Command+Q',
+                    click() {
+                        app.quit()
                     },
-                ],
-            },
-            {
-                label: 'Edit',
-                submenu: [
-                    { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-                    { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-                    { type: 'separator' },
-                    { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-                    { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-                    { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-                    { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
-                ],
-            },
-            {
-                label: 'Window',
-                submenu: [
-                    { label: 'Minimize', accelerator: 'Command+M', selector: 'performMiniaturize:' },
-                    { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
-                    { type: 'separator' },
-                    { label: 'Bring All to Front', selector: 'arrangeInFront:' },
-                ],
-            },
-        ]))
-    },
+                },
+            ],
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+                { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+                { type: 'separator' },
+                { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+                { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+                { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+                { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+            ],
+        },
+        {
+            label: 'Window',
+            submenu: [
+                { label: 'Minimize', accelerator: 'Command+M', selector: 'performMiniaturize:' },
+                { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
+                { type: 'separator' },
+                { label: 'Bring All to Front', selector: 'arrangeInFront:' },
+            ],
+        },
+    ]))
 }
