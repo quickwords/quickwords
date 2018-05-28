5016c4c5277c5180c6e69f74c3ac3421ce09ed61
gtluszcz <gtluszcz@gmail.com>
2018-05-28 11:55:09 +0200

    removed checkForNewVersion from helpers.js

--- src/main/helpers.js
+++ src/main/helpers.js
@@ -13,43 +13,6 @@ module.exports.unregisterWindowListeners = function (windows) {
     Object.keys(windows).forEach(key => windows[key].removeAllListeners('close'))
 }
 
-module.exports.checkForNewVersion = async function () {
-    const currentVersion = require('../../package.json').version.split('.')
-
-    let response
-    let data
-
-    try {
-        response = await fetch('https://api.github.com/repos/quickwords/quickwords/releases/latest')
-        data = await response.json()
-    } catch (e) {
-        return false
-    }
-
-    const currentNewestVersion = data.tag_name.split('.')
-    const url = data.html_url
-
-    if (
-        currentNewestVersion[0] > currentVersion[0]
-        || (currentNewestVersion[0] === currentVersion[0] && currentNewestVersion[1] > currentVersion[1])
-        || (currentNewestVersion[0] === currentVersion[0] && currentNewestVersion[1] === currentVersion[1] && currentNewestVersion[2] > currentVersion[2])
-    ) {
-        const notification = new Notification({
-            title: 'New Version Available',
-            body: `Version ${currentNewestVersion.join('.')} of Quickwords is available`,
-            icon: path.join(__dirname, '../../build/icon.icns'),
-        })
-
-        notification.on('click', () => shell.openExternal(url))
-
-        notification.show()
-
-        return true
-    }
-
-    return false
-}
-
 module.exports.registerNativeShortcuts = function (app, windows) {
     // This menu does not show up nowhere, but it does register shortcuts like copy-paste, close and minimize
     Menu.setApplicationMenu(Menu.buildFromTemplate([
