f3baaa70c1a8444dd60a2349602ec287a83d97a5
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-28 12:22:04 +0200

    Fix default snippets

--- src/main/modules/defaultSnippets.js
+++ src/main/modules/defaultSnippets.js
@@ -14,9 +14,9 @@ module.exports = [
         'type': 'plain',
     },
     {
-        'id': 4,
+        'id': 3,
         'key': ';up .* ',
-        'value': '// After typing everything after \';up \' becomes uppercase\n// Note that Quickwords matches only last 20\n// characters, so anything above that will not work\nfunction (trigger) {\n    return trigger.substr(4).toUpperCase()\n}\n',
+        'value': '// After typing everything after \';up \' becomes uppercase\n// Note that Quickwords matches only last 20\n// characters by default, so anything longer than that will not work\nfunction qw(trigger) {\n    return trigger.substr(4).toUpperCase()\n}\n',
         'regex': true,
         'type': 'js',
     },
